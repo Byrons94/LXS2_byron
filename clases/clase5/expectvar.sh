9fe2695875dc70f4adad435b75f55deafa74ca36
Carlos <calinrv17@gmail.com>
2016-10-08 11:46:02 -0600

    subiendo expectcar.sh

---
+++ clases/clase5/expectvar.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+#primero revisamos que el numero de parametros sea el correcto
+#--------------------------------------------------
+#Revision de argumentos
+#son 3 argumentos
+
+ARGS=3
+if [ $# -ne "$ARGS" ]
+then
+	echo "Error:"
+	echo "Uso: $0 <op1> <op2> <op3>"
+	exit 1
+fi
+#--------------------------------------------------
+
+#Quiero ver cuales son los argumentos
+
