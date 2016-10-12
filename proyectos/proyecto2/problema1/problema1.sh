6891f9e0acccd535866982c28b9a11f331651415
Byron Serrano <bironjosue@hotmail.com>
2016-10-12 16:55:41 -0600

    terminando ejercicio 1  problema a la hora de generar el plot, mensaje dice illegal month

--- proyectos/proyecto2/problema1/problema1.sh
+++ proyectos/proyecto2/problema1/problema1.sh
@@ -2,10 +2,12 @@
 
 #se establece la ruta donde se encuentran los arrchivos .xls
 DATA=./hojasDatos
-
-mkdir $DATA/datos_csv
-
 OUT_DATA=$DATA/datos_csv
+GRAF_DATA=$DATA/datos_graf
+
+mkdir $DATA/archivos_csv
+mkdir $GRAF_DATA
+
 m=0
 
 for i in `find $DATA -name '*.xls'`
@@ -14,3 +16,14 @@ do
 	sudo xls2csv $i > $OUT_DATA/data-$m.csv
 	let m=m+1
 done 2> error1.log
+
+m=0
+
+for e in `find $OUT_DATA -name "*.csv"`
+do
+	echo "Dando formato de datos para graficar el archivo $e"
+	
+	cat $e | awk -F "\",\"" '{print $1 " " $2 " " $3 " " $4 " " $5}' | sed '1,$ s/"//g' | sed '1 s/date/$date/g' > $GRAF_DATA/graf-$m.dat
+	
+	let m=m+1
+done 2> error2.log
